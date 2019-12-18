3db6693ffad4da4ae3c237cec8ae5d079310d07f
Kenny Kelley <16879853+kenny-kelley@users.noreply.github.com>
2019-12-18 15:06:00 -0500

    Starting point for a random number generator

---
+++ rng.py
@@ -0,0 +1,25 @@
+import argparse
+
+"""
+Author: Kenny Kelley
+Email: kelley.796@osu.edu
+Date: 2019-18-12
+
+"""
+
+
+def do_argparse():
+    parser = argparse.ArgumentParser(description='Generates a random integer.')
+    # TODO
+    return arg_parser.parse_args()
+
+
+def main():
+
+    args = do_argparse()
+
+    # TODO
+
+
+if __name__ == '__main__':
+    main()
