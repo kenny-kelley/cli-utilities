5089429b4d546867d69607cb99eb07ba92e281c3
Kenny Kelley <16879853+kenny-kelley@users.noreply.github.com>
2019-12-18 15:42:28 -0500

    Implementing the random number generator

--- rng.py
+++ rng.py
@@ -1,24 +1,31 @@
 import argparse
+import sys
+import random
+import os
 
-"""
+'''
 Author: Kenny Kelley
 Email: kelley.796@osu.edu
 Date: 2019-18-12
 
-"""
+'''
 
 
 def do_argparse():
-    parser = argparse.ArgumentParser(description='Generates a random integer.')
-    # TODO
-    return arg_parser.parse_args()
+    parser = argparse.ArgumentParser(description='Generates a random integer.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-a', '--start', help='inclusive lower bound for RNG pool', type=int, default=0)
+    parser.add_argument('-b', '--stop', help='inclusive upper bound for RNG pool', type=int, default=sys.maxsize)
+    parser.add_argument('--seed_size', help='size in bytes of the seed the OS generates via os.urandom()', default=1024)
+    return parser.parse_args()
 
 
 def main():
 
     args = do_argparse()
 
-    # TODO
+    seed = os.urandom(args.seed_size);
+    generator = random.seed(a=seed, version=2)
+    print(random.randrange(args.start, args.stop))
 
 
 if __name__ == '__main__':
