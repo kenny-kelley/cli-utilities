9a6dd1a74082d242a4c1b2db1383f08f3bf97c69
Kenny Kelley <16879853+kenny-kelley@users.noreply.github.com>
2019-12-18 14:49:56 -0500

    Changing ArgumentParser object name

--- get-key-count.py
+++ get-key-count.py
@@ -9,10 +9,10 @@ Date: 2019-17-12
 
 
 def do_argparse():
-    arg_parser = argparse.ArgumentParser(description='Prints the number of unique strings '
+    parser = argparse.ArgumentParser(description='Prints the number of unique strings '
         + 'separated by newlines in a file.')
-    arg_parser.add_argument('file', help='path to a valid file')
-    return arg_parser.parse_args()
+    parser.add_argument('file', help='path to a valid file')
+    return parser.parse_args()
 
 
 def main():
