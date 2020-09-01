b57c4c18581ef4a61ee21240b620144b5de4541c
Kenny Kelley <16879853+kenny-kelley@users.noreply.github.com>
2020-08-31 20:47:29 -0400

    Initial implementation

--- strip-vowels.py
+++ strip-vowels.py
@@ -6,20 +6,26 @@ Date: 2020-08-31
 
 """
 
+VOWELS = ('a', 'e', 'i', 'o', 'u')
+
 
 def do_argparse():
     parser = argparse.ArgumentParser(description='Removes all of the vowels from a given string.')
-    parser.add_argument('string', help='a string you would like to have the vowels removed from')
+    parser.add_argument('input_str', help='a string you would like to have the vowels removed from')
     return parser.parse_args()
 
 
-def strip_vowels(my_str):
-    return my_str
+def strip_vowels(input_str):
+    output_str = ''
+    for c in input_str:
+        if c.lower() not in VOWELS:
+            output_str += c
+    return output_str
 
 
 def main():
     args = do_argparse()
-    print(strip_vowels(args.string))
+    print(strip_vowels(args.input_str))
 
 
 if __name__ == '__main__':
