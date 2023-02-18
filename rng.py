220f6b530d7858141e2e81421309ab1fece7c09d
Kenny Kelley <16879853+kenny-kelley@users.noreply.github.com>
2023-02-18 00:11:10 -0500

    Updating some CLI argument names in the RNG script

--- rng.py
+++ rng.py
@@ -14,8 +14,8 @@ Date: 2019-12-18
 
 def do_argparse():
     parser = argparse.ArgumentParser(description='Generates a random integer.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-a', '--start', help='inclusive lower bound for RNG pool', type=int, default=0)
-    parser.add_argument('-b', '--stop', help='inclusive upper bound for RNG pool', type=int, default=sys.maxsize)
+    parser.add_argument('-a', '--lower_bound', help='inclusive lower bound for RNG pool', type=int, default=0)
+    parser.add_argument('-b', '--upper_bound', help='inclusive upper bound for RNG pool', type=int, default=sys.maxsize)
     parser.add_argument('--seed_size', help='size in bytes of the seed the OS generates via os.urandom()', default=1024)
     return parser.parse_args()
 
@@ -23,7 +23,7 @@ def do_argparse():
 def main():
     args = do_argparse()
     random.seed(a=os.urandom(args.seed_size), version=2)
-    print(random.randrange(args.start, args.stop))
+    print(random.randrange(args.lower_bound, args.upper_bound))
 
 
 if __name__ == '__main__':
