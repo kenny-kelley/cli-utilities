42d38482483d652ab701da6edb6f85a8b0ccb44e
Kenny Kelley <16879853+kenny-kelley@users.noreply.github.com>
2023-03-29 22:45:59 -0400

    I wrote this over a month ago but I forgot to commit it

--- meme-speak.py
+++ meme-speak.py
@@ -1,27 +1,40 @@
 #!/usr/bin/env python3
 
 import argparse
+import random
 
 """
 Author: Kenny Kelley
-Date: 2020-08-31
+Date: 2023-02-18
 
 """
 
 
 def do_argparse():
     parser = argparse.ArgumentParser(description="wHat Do yOU tHiNk iT DOes?")
-    parser.add_argument("string", help="a string you would like to memeify")
+    parser.add_argument("strings", metavar="string", help="a string you would like to memeify", nargs="+")
     return parser.parse_args()
 
 
-def memeify(my_str):
-    return my_str
+def memeify(my_str: str) -> str:
+    char_list = list(my_str)
+    for i, char in enumerate(char_list):
+        if i > 2:
+            if random.randint(0, 1) == 0 and char_list[i - 1].isupper() or char_list[i - 2].isupper():
+                char_list[i] = char.lower()
+            else:
+                char_list[i] = char.upper()
+        else:
+            if random.randint(0, 1) == 0:
+                char_list[i] = char.lower()
+            else:
+                char_list[i] = char.upper()
+    return "".join(char_list)
 
 
 def main():
     args = do_argparse()
-    print(memeify(args.string))
+    print(memeify(" ".join(args.strings)))
 
 
 if __name__ == "__main__":
