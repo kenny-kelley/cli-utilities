fe60a5d6ab910ece38cc47cdfc8b2c4fbc173290
Kenny Kelley <16879853+kenny-kelley@users.noreply.github.com>
2023-08-05 01:45:36 -0400

    Adding B button emoji script

---
+++ b-emoji.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import argparse
+import re
+
+"""
+Author: Kenny Kelley
+Date: 2023-08-05
+
+"""
+
+B_EMOJI = "\U0001f171\ufe0f "
+
+
+def do_argparse():
+    parser = argparse.ArgumentParser(description=B_EMOJI)
+    parser.add_argument("strings", metavar="string", help=B_EMOJI, nargs="+", type=str)
+    return parser.parse_args()
+
+
+def main():
+    args = do_argparse()
+    print(re.sub("[bB]", B_EMOJI, " ".join(args.strings)))
+
+
+if __name__ == "__main__":
+    main()
